ee3cfa9118d69a2574d7f665017b44ecd2ac91e7
Yaroslav82 <yargay230607@gmail.com>
2023-10-12 14:44:49 +0300

    Create AppStarter

---
+++ src/main/java/org/example/app/utils/AppStarter.java
@@ -0,0 +1,18 @@
+package org.example.app.utils;
+
+import org.example.app.conf.AppConfig;
+import org.example.app.controller.ProductControllerImpl;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class AppStarter {
+
+    public static void startApp() {
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        ProductControllerImpl contactController = context.getBean(ProductControllerImpl.class);
+        contactController.getOption();
+
+        context.close();
+    }
+}
